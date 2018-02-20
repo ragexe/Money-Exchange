16f0ea1cb667853d028088a015c651f7e1c65321
ragexe <ragexe@mail.ru>
2018-02-21 02:13:41 +0300

    money-exchange/final

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+    let result = {};
+    if (!currency) return result;
+    if (currency > 10000) return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+
+    const config = {"H": 50, "Q": 25, "D": 10, "N": 5, "P": 1};
+
+    while (currency > 0) {
+        for (let a in config) {
+            if (config[a] <= currency) {
+                result[a] = result[a] || 0;
+                result[a] += 1;
+                currency -= config[a];
+                break;
+            }
+        }
+    }
+
+    return result;
+};
+
+
+
+
